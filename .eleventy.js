70b6915a73ab06242555d75cfff707f228281ed9
David Darnes <me@daviddarnes.com>
2019-09-28 16:12:38 +0100

    Add plugins and styling tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,45 @@
+const pluginRSS = require("@11ty/eleventy-plugin-rss");
+const localImages = require("eleventy-plugin-local-images");
+const lazyImages = require("eleventy-plugin-lazyimages");
+const htmlmin = require('html-minifier');
+
+const htmlMinTransform = (value, outputPath) => {
+  if (outputPath.indexOf('.html') > -1) {
+    let minified = htmlmin.minify(value, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true,
+      minifyCSS: true
+    });
+    return minified;
+  }
+  return value;
+};
+
 module.exports = function(config) {
+
+  // Merge default an theme specific tags together
   config.setDataDeepMerge(true);
+
+  // Minify HTML
+  config.addTransform("htmlmin", htmlMinTransform);
+
+  // Assist RSS feed template
+  config.addPlugin(pluginRSS);
+
+  // Apply performance attributes to images
+  config.addPlugin(lazyImages, {
+    cacheFile: ""
+  });
+
+  // Copy images over from Ghost
+  config.addPlugin(localImages, {
+    distPath: "dist",
+    assetPath: "/assets/images",
+    selector: "img",
+    attribute: "data-src", // Lazy images attribute
+    verbose: false
+  });
 
   return {
     dir: {
